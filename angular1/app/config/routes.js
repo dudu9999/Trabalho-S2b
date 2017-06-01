12d542f6c1f96eea6de808c9e9acffc605840fd5
Eduardo Caetano <ecaetanocorrea@gmail.com>
2017-06-01 17:11:45 -0300

    correção router 1

--- angular1/app/config/routes.js
+++ angular1/app/config/routes.js
@@ -1,8 +1,8 @@
 angular.module('primeiroApp').config([
   '$stateProvider',
   '$urlRouterProvider',
-  function($stateProvider, urlRouterProvider){
-    $stateProvider.state('dashboard',{
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('dashboard', {
       url: "/dashboard",
       templateUrl: "dashboard/dashboard.html"
     }).state('cicloCompra',{
